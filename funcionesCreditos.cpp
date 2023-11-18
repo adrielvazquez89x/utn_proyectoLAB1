6892d955d8768e62526d7f904114f49a74b9b765
Adriel Vazquez <adrialvaz@hotmail.com>
2023-11-18 09:59:47 -0300

    update credits

--- funcionesCreditos.cpp
+++ funcionesCreditos.cpp
@@ -10,109 +10,114 @@
 
 using namespace std;
 
-void reproducirSonidoCred() {
+void reproducirSonidoCred()
+{
     PlaySound(TEXT("Level-Complete.wav"), NULL, SND_ASYNC);
 }
 
 void mostrarCreditos(int &opcion, Jugador &j1, Jugador &j2)
 {
-  setlocale(LC_ALL, "Spanish");
-  rlutil::hidecursor(); // Escondemos el cursor en la consola
+    setlocale(LC_ALL, "Spanish");
+    rlutil::hidecursor(); // Escondemos el cursor en la consola
 
-  // Inicia un hilo para reproducir el sonido
-  thread sonido(reproducirSonidoCred);
+    // Inicia un hilo para reproducir el sonido
+    thread sonido(reproducirSonidoCred);
 
-  int pantallaAltura = rlutil::trows(); //La docu dice que te da el número de filas de la terminal.
-  int pantallaAncho = rlutil::tcols();  // Lo mismo, pero las columnas.
-  int x = pantallaAncho / 2;            // Hacemos uso de las clases de matemática lo dividimos a la mitad para ubicar el centro
-  int y = pantallaAltura;
+    int pantallaAltura = rlutil::trows(); //La docu dice que te da el número de filas de la terminal.
+    int pantallaAncho = rlutil::tcols();  // Lo mismo, pero las columnas.
+    int x = pantallaAncho / 2;            // Hacemos uso de las clases de matemática lo dividimos a la mitad para ubicar el centro
+    int y = pantallaAltura;
 
-  string creditos = "CREDITOS";
-  string alumnos = "Alumnos: ";
-  string miguel = "Lardo Miguel Angel - LEGAJO: 29812";
-  string adriel = "Vazquez Adriel Alejandro - LEGAJO: 29930";
-  string equipo = "Equipo n° 33 - Maestros de la luz y de las sombras.";
+    string creditos = "CREDITOS";
+    string alumnos = "Alumnos: ";
+    string miguel = "Lardo Miguel Angel - LEGAJO: 29812";
+    string anita = "Leyez Analia Belen - LEGAJO: 29817";
+    string adriel = "Vazquez Adriel Alejandro - LEGAJO: 29930";
+    string equipo = "Equipo n° 33 - Maestros de la luz y de las sombras.";
 
-  while (y >= pantallaAltura / 2) // Acá lo mismo, dividimos por dos para ubicar el centro.
-  {
+    while (y >= pantallaAltura / 2) // Acá lo mismo, dividimos por dos para ubicar el centro.
+    {
 
-    // En la "x" y en la "y", tenemos como el eje, entonces, le vamos restando o sumando para ir ubicando cada dos espacios.
-    rlutil::cls();
-    rlutil::locate(x, y-6 );
-    cout << creditos << endl;
+        // En la "x" y en la "y", tenemos como el eje, entonces, le vamos restando o sumando para ir ubicando cada dos espacios.
+        rlutil::cls();
+        rlutil::locate(x, y-6 );
+        cout << creditos << endl;
+        rlutil::locate(x, y -4);
+        cout << alumnos << endl;
+        rlutil::locate(x - 12, y -2);
+        cout << miguel << endl;
+        rlutil::locate(x - 12, y );
+        cout << anita << endl;
+        rlutil::locate(x - 18, y +2 );
+        cout << adriel;
+        rlutil::locate(x - 26, y + 4);
+        cout << equipo;
+
+        rlutil::msleep(50); // Esta funcion, espera en milisegundos para ejecutar. A mayor número mas tarda.
+        y -= 2;
+
+        rlutil::cls(); // Esto es lo que da el efecto de "subida" en ralidad, borra y escribe.
+    }
+
+    rlutil::cls(); //Esto es para limpiar
+
+    // Todo este bloque lo volvemos a escribir, para generar el efecto posterior.
     rlutil::locate(x, y -4);
     cout << alumnos << endl;
     rlutil::locate(x - 12, y -2);
     cout << miguel << endl;
-    rlutil::locate(x - 16, y );
+    rlutil::locate(x - 12, y );
+    cout << anita << endl;
+    rlutil::locate(x - 18, y +2 );
     cout << adriel;
-    rlutil::locate(x - 24, y + 2);
+    rlutil::locate(x - 26, y + 4);
     cout << equipo;
+    rlutil::setColor(rlutil::BLACK);
 
-    rlutil::msleep(50); // Esta funcion, espera en milisegundos para ejecutar. A mayor número mas tarda.
-    y -= 2;
+    /*
+        En la docu, encontré esto:
 
-    rlutil::cls(); // Esto es lo que da el efecto de "subida" en ralidad, borra y escribe.
-  }
+    0 Black
+    1 Blue (ANSI red)
+    2 Green
+    3 Cyan (ANSI brown)
+    4 Red (ANSI blue)
+    5 Magenta
+    6 Brown (dark yellow)
+    7 Light Grey (white)
+    8 Grey (dark grey or light black)
+    9 Light blue (ANSI light red)
+    10 Light green
+    11 Light cyan (ANSI yellow)
+    12 Light red (ANSI light magenta)
+    13 Light magenta
+    14 Yellow (ANSI light cyan)
+    15 White (bright)
+    */
 
-  rlutil::cls(); //Esto es para limpiar
+    //INCREIBLEMENTE FUNCIONA
+    int colores[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
 
-  // Todo este bloque lo volvemos a escribir, para generar el efecto posterior.
-  rlutil::locate(x, y -4);
-  cout << alumnos << endl;
-  rlutil::locate(x - 12, y -2);
-  cout << miguel << endl;
-  rlutil::locate(x - 16, y );
-  cout << adriel;
-  rlutil::locate(x - 24, y + 2);
-  rlutil::setColor(rlutil::RED);
-  cout << equipo;
-  rlutil::setColor(rlutil::BLACK);
+    //https://www.lawebdelprogramador.com/foros/Dev-C/1664435-Kbhit-y-getch.html
+    //https://tapiov.net/rlutil/docs/HTML/files/rlutil-h.html
 
-  /*
-      En la docu, encontré esto:
-
-  0 Black
-  1 Blue (ANSI red)
-  2 Green
-  3 Cyan (ANSI brown)
-  4 Red (ANSI blue)
-  5 Magenta
-  6 Brown (dark yellow)
-  7 Light Grey (white)
-  8 Grey (dark grey or light black)
-  9 Light blue (ANSI light red)
-  10 Light green
-  11 Light cyan (ANSI yellow)
-  12 Light red (ANSI light magenta)
-  13 Light magenta
-  14 Yellow (ANSI light cyan)
-  15 White (bright)
-  */
-
-  //INCREIBLEMENTE FUNCIONA
-  int colores[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-
-  //https://www.lawebdelprogramador.com/foros/Dev-C/1664435-Kbhit-y-getch.html
-  //https://tapiov.net/rlutil/docs/HTML/files/rlutil-h.html
-
-  while (!kbhit()) // Esta función está incluida en rlutil, detecta si presionas una tecla. Por alguna razón si uso rlutil:: antes, no funciona.
-  {
-    for (int i = 0; i < 8 ; i++)
+    while (!kbhit()) // Esta función está incluida en rlutil, detecta si presionas una tecla. Por alguna razón si uso rlutil:: antes, no funciona.
     {
-      rlutil::locate(x, y - 6);  // Ubica la letra actual
-      rlutil::setColor(colores[i]); // Cambia el color
-      cout << creditos; // Imprime la letra
-      rlutil::msleep(50); // Espera un tiempo antes de continuar
+        for (int i = 0; i < 8 ; i++)
+        {
+            rlutil::locate(x, y - 6);  // Ubica la letra actual
+            rlutil::setColor(colores[i]); // Cambia el color
+            cout << creditos; // Imprime la letra
+            rlutil::msleep(50); // Espera un tiempo antes de continuar
+        }
     }
-  }
-  getch(); // Esta función, es compañera de kbhit, si no la agrego, lo que sigue  no se muestra.
+    getch(); // Esta función, es compañera de kbhit, si no la agrego, lo que sigue  no se muestra.
 
-  rlutil::setColor(rlutil::BLACK);
-  rlutil::locate(x - 20, y +6);
-  cout << "Presiona una tecla para volver al menu principal";
-  rlutil::getkey();
-  rlutil::cls();
-  PlaySound(NULL, 0, 0);
-  sonido.join();//esta es para que el programa no se rompa al salir de CREDITOS
+    rlutil::setColor(rlutil::BLACK);
+    rlutil::locate(x - 20, y +6);
+    cout << "Presiona una tecla para volver al menu principal";
+    rlutil::getkey();
+    rlutil::cls();
+    PlaySound(NULL, 0, 0);
+    sonido.join();//esta es para que el programa no se rompa al salir de CREDITOS
 }
